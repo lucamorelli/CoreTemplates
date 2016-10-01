a26c7dbb3c112c5b1b474394abbd44db74857a26
luca morelli <luca@lucamorelli.it>
2016-10-01 10:54:31 +0200

    Add project files.

---
+++ src/Angular/gulpfile.js
@@ -0,0 +1,64 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+var paths = {};
+
+paths.webroot = "./wwwroot/";
+paths.npmSrc = "./node_modules/";
+paths.npmLibs = paths.webroot + "libs";
+
+gulp.task("copy-deps:@angular", function () {
+    return gulp.src(paths.npmSrc + '/@angular/**/*.{js,map,json}', { base: paths.npmSrc + '/@angular/' })
+         .pipe(gulp.dest(paths.npmLibs + '/@angular/'));
+});
+
+gulp.task("copy-deps:core-js", function () {
+    return gulp.src(paths.npmSrc + '/core-js/client/**/*.{js,map}', { base: paths.npmSrc + '/core-js/client/' })
+         .pipe(gulp.dest(paths.npmLibs + '/core-js/'));
+});
+
+gulp.task("copy-deps:reflect-metadata", function () {
+    return gulp.src(paths.npmSrc + '/reflect-metadata/*.{js,map}', { base: paths.npmSrc + '/reflect-metadata/' })
+         .pipe(gulp.dest(paths.npmLibs + '/reflect-metadata/'));
+});
+
+gulp.task("copy-deps:rxjs", function () {
+    return gulp.src(paths.npmSrc + '/rxjs/**/*.{js,map}', { base: paths.npmSrc + '/rxjs/' })
+         .pipe(gulp.dest(paths.npmLibs + '/rxjs/'));
+});
+
+gulp.task("copy-deps:systemjs", function () {
+    return gulp.src(paths.npmSrc + '/systemjs/dist/*.*', { base: paths.npmSrc + '/systemjs/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/systemjs/'));
+});
+
+gulp.task("copy-deps:zone.js", function () {
+    return gulp.src(paths.npmSrc + '/zone.js/dist/**/*.*', { base: paths.npmSrc + '/zone.js/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/zone.js/'));
+});
+
+gulp.task("copy-deps:bootstrap", function () {
+    return gulp.src(paths.npmSrc + '/bootstrap/dist/**/*.*', { base: paths.npmSrc + '/bootstrap/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/bootstrap/'));
+});
+
+gulp.task("copy-deps:jquery", function () {
+    return gulp.src(paths.npmSrc + '/jquery/dist/*.*', { base: paths.npmSrc + '/jquery/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/jquery/'));
+});
+
+gulp.task("clean-deps", function () {
+    return del([paths.npmLibs + '/**/**']);;
+});
+
+gulp.task("copy-deps", ['copy-deps:@angular', 'copy-deps:core-js', 'copy-deps:reflect-metadata',
+    'copy-deps:rxjs', 'copy-deps:systemjs', 'copy-deps:zone.js', 'copy-deps:bootstrap', 'copy-deps:jquery' ]);
---
+++ src/Aurelia/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+
+namespace Aurelia
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc()
+                // uncomment this if you want that serializing objects in the api results the casing is respected
+                //.AddJsonOptions(options => {
+
+                //    var resolver = options.SerializerSettings.ContractResolver;
+                //    if (resolver != null)
+                //    {
+                //        var res = resolver as DefaultContractResolver;
+                //        res.NamingStrategy = null;  // <<!-- this removes the camelcasing
+                //    }
+                //})
+                ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            var options = new DefaultFilesOptions
+            {
+                DefaultFileNames = new[] { "default.html", "index.html" }
+            };
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
