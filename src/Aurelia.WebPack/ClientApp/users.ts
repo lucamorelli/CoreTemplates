8ae0e0aba0637f9c3fb24b616f8e087cdfe1c206
luca morelli <luca@lucamorelli.it>
2016-10-23 15:23:05 +0200

    adding aurelia + webpack simple, hot reloading not working

---
+++ src/Aurelia.WebPack/ClientApp/users.ts
@@ -0,0 +1,25 @@
+/// <reference path="../node_modules/aurelia-fetch-client/doc/url.d.ts" />
+/// <reference path="../node_modules/aurelia-fetch-client/doc/whatwg-fetch.d.ts" />
+
+import { autoinject } from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+
+@autoinject
+export class Users {
+  public heading = 'Github Users';
+  public users = [];
+
+  constructor(private http: HttpClient) {
+    http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl('https://api.github.com/');
+    });
+  }
+
+  public activate() {
+    return this.http.fetch('users')
+      .then(response => response.json())
+      .then(users => this.users = users);
+  }
+}
